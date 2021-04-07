8fc45c5e74a12c6184f827f7bb5fc8b5126d3409
Carlos Del Carpio <cdeclarp@uncc.edu>
2021-04-07 19:29:41 -0400

    starting login screen

--- InstaPhotos/app/src/main/java/com/example/instaphotos/MainActivity.java
+++ InstaPhotos/app/src/main/java/com/example/instaphotos/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.instaphotos;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
