37e81b06c64dae6df66d083133c2f5e121a7b66d
Carlos Del Carpio <cdeclarp@uncc.edu>
2021-04-08 07:34:01 -0400

    added dependencies for FireBase

--- InstaPhotos/app/src/main/java/com/example/instaphotos/Login.java
+++ InstaPhotos/app/src/main/java/com/example/instaphotos/Login.java
@@ -6,15 +6,30 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class Login extends Fragment {
+    EditText email;
+    EditText password;
+    Button login;
+    TextView createNewAccount;
+
+
     public Login() {}
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,  Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+
+        email = view.findViewById(R.id.inputEmailLogin);
+        password = view.findViewById(R.id.passwordLogin);
+        login = view.findViewById(R.id.buttonLogin);
+        createNewAccount = view.findViewById(R.id.createNewAccount);
 
 
         return view;
