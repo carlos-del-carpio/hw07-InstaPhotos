ad11f36fdd8bf0da45cf3134e8e0da63da3801d2
Carlos Del Carpio <cdeclarp@uncc.edu>
2021-04-11 16:14:32 -0400

    added ability to upload image to FireStorage

--- InstaPhotos/app/src/main/java/com/example/instaphotos/Timeline.java
+++ InstaPhotos/app/src/main/java/com/example/instaphotos/Timeline.java
@@ -2,23 +2,41 @@ package com.example.instaphotos;
 
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import java.util.UUID;
+
+import static android.app.Activity.RESULT_OK;
 
 
 public class Timeline extends Fragment {
+    FirebaseStorage storage = FirebaseStorage.getInstance();
+    final String TAG = "Carlos";
+    static final int REQUEST_IMAGE_GET = 714;
     TimelineListener timelineListener;
     ImageView logout;
     Button friends;
     Button add;
     RecyclerView recycler;
+    LinearLayoutManager linearLayoutManager;
 
 
     public Timeline() {}
@@ -34,6 +52,7 @@ public class Timeline extends Fragment {
         friends = view.findViewById(R.id.buttonFriends);
         add = view.findViewById(R.id.buttonAdd);
         recycler = view.findViewById(R.id.recyclerUserPhotos);
+        recycler.setLayoutManager(linearLayoutManager);
 
 
         friends.setOnClickListener(new View.OnClickListener() {
@@ -47,7 +66,8 @@ public class Timeline extends Fragment {
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Log.d(TAG, "onClick: ");
+                getPictureFromCameraRoll();
             }
         });
 
@@ -65,6 +85,49 @@ public class Timeline extends Fragment {
 
 
     @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d(TAG, "onActivityResult: got back to activity");
+        Log.d(TAG, "onActivityResult: " + requestCode);
+        Log.d(TAG, "onActivityResult: " + resultCode);
+        Log.d(TAG, "onActivityResult: " + data.toString());
+        if (requestCode == REQUEST_IMAGE_GET && resultCode == RESULT_OK) {
+            Log.d(TAG, "onActivityResult: ");
+            if (requestCode == REQUEST_IMAGE_GET && resultCode == RESULT_OK && data.getData() != null) {
+                Log.d(TAG, "onActivityResult: we got data");
+                Uri selectedImage = data.getData();
+                uploadImage(selectedImage);
+            }
+        }
+    }
+
+
+    void uploadImage(Uri image) {
+        final String randomID = UUID.randomUUID().toString();
+        String uploadPath = FirebaseAuth.getInstance().getUid().toString() + "/" + randomID;
+        Log.d(TAG, "uploadImage: " + uploadPath);
+        StorageReference storageRef = storage.getReference();
+        StorageReference riversRef = storageRef.child(uploadPath);
+
+
+        riversRef.putFile(image).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Log.d(TAG, "onSuccess: ");
+                Snackbar.make(getActivity().findViewById(android.R.id.content), "image uploaded", Snackbar.LENGTH_LONG).show();
+            }
+        })
+        .addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG, "onFailure: ");
+                Snackbar.make(getActivity().findViewById(android.R.id.content), "image failed to upload", Snackbar.LENGTH_LONG).show();
+            }
+        });
+    }
+
+
+
+    @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
 
@@ -74,6 +137,19 @@ public class Timeline extends Fragment {
         } else {
             throw new RuntimeException(context.toString());
         }
+    }
+
+
+    void getPictureFromCameraRoll() {
+        Log.d(TAG, "getPictureFromCameraRoll: ");
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+
+
+//        if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivityForResult(intent, REQUEST_IMAGE_GET);
+//        }
     }
 
 
